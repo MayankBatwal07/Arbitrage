fa8d69ff81a224a653126561ffd67550479e8cae
MayankBatwal07 <mayankbatwal.jecrc@gmail.com>
2024-11-09 02:02:55 +0530

    Complete Flash Loan

---
+++ ignition/modules/Lock.js
@@ -0,0 +1,48 @@
+// const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+// const JAN_1ST_2030 = 1893456000;
+// const ONE_GWEI = 1_000_000_000n;
+
+// module.exports = buildModule("LockModule", (m) => {
+//   const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
+//   const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
+
+//   const lock = m.contract("Lock", [unlockTime], {
+//     value: lockedAmount,
+//   });
+
+//   return { lock };
+// });
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime = currentTimestampInSeconds + 60;
+
+  const lockedAmount = hre.ethers.parseEther("0.001");
+
+  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+    value: lockedAmount,
+  });
+
+  await lock.waitForDeployment();
+
+  console.log(
+    `Lock with ${ethers.formatEther(
+      lockedAmount
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
